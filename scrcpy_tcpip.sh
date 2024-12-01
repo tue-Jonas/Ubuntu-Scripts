7b2c38417aad6fcecf6d9575cdaabec629e18883
Jonas Tüchler <jonas.tuechler0@gmail.com>
2024-12-01 11:22:26 +0100

    improved script

--- scrcpy_tcpip.sh
+++ scrcpy_tcpip.sh
@@ -1,2 +1,7 @@
-adb connect $PHONE_IP:$(nmap $PHONE_IP -p 37000-65535 | awk "/\/tcp/" | cut -d/ -f1);
-scrcpy -K --window-x 3840 --window-y 0 --window-height 1080 --window-width 469 -m1920 --max-fps=60 --select-tcpip
+#!/bin/bash
+
+PHONE_IP="192.168.1.101"
+
+adb connect $PHONE_IP:$(nmap $PHONE_IP -p 30000-65535 | awk '/\/tcp.*open/ {print $1}' | cut -d/ -f1 | head -n 1);
+sleep 2;
+scrcpy -K --window-x 3840 --window-y 0 --window-height 1080 --window-width 469 -m1920 --max-fps=60 --select-tcpip;
