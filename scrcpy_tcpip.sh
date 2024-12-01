27367557cab8130d8ec75d5f7f1a295cb2fc4264
Jonas Tüchler <jonas.tuechler0@gmail.com>
2024-12-01 11:24:37 +0100

    check if adb already connected

--- scrcpy_tcpip.sh
+++ scrcpy_tcpip.sh
@@ -2,6 +2,17 @@
 
 PHONE_IP="192.168.1.101"
 
-adb connect $PHONE_IP:$(nmap $PHONE_IP -p 30000-65535 | awk '/\/tcp.*open/ {print $1}' | cut -d/ -f1 | head -n 1);
-sleep 2;
-scrcpy -K --window-x 3840 --window-y 0 --window-height 1080 --window-width 469 -m1920 --max-fps=60 --select-tcpip;
+# Check if the device is already connected
+if adb devices | grep -q "$PHONE_IP"; then
+    echo "Device already connected. Launching scrcpy..."
+else
+    # Find an open port and connect to the device
+    echo "Connecting to device..."
+    PORT=$(nmap $PHONE_IP -p 30000-65535 | awk '/\/tcp.*open/ {print $1}' | cut -d/ -f1 | head -n 1)
+    adb connect $PHONE_IP:$PORT
+    sleep 2
+fi
+
+# Execute scrcpy
+scrcpy -K --window-x 3840 --window-y 0 --window-height 1080 --window-width 469 -m1920 --max-fps=60 --select-tcpip
+
